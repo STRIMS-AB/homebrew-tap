8bef12fed68c1c499c4cb5bd48c7acec8da8c4ee
kamaldevj <kelbed@gmail.com>
2026-02-09 23:12:32 +0100

    Simplify workflow 1

--- bbrun.rb
+++ bbrun.rb
@@ -5,30 +5,26 @@
 class Bbrun < Formula
   desc "HTTP API testing DSL - write expressive API tests"
   homepage "https://github.com/STRIMS-AB/bbrun"
-  version "0.1.3"
+  version "0.1.7"
   license "MIT"
 
   url "https://github.com/STRIMS-AB/bbrun/releases/download/v#{version}/bbrun-#{version}.tar.gz"
-  sha256 "33d361c952ee7e3cce5fd11e230bad98a9644a91a8e0af278c115bb0c2bb40c8"
+  sha256 "8d775bef196ed59d06de1e7dcb3b18470a5a37ca91d48d8699a55faf1760b919"
 
   depends_on "openjdk@17"
 
   def install
-    # The tarball extracts to bbrun-0.1.1/ which contains bin/ and lib/
-    # Install everything to libexec, preserving the structure
+    # Install everything to libexec
     libexec.install Dir["*"]
     
-    # Remove Windows batch files
-    rm_f Dir[libexec/"bin/*.bat"]
-    
     # Make the script executable
-    chmod 0755, libexec/"bin/bbrun-cli"
+    chmod 0755, libexec/"bin/bbrun"
     
-    # Create wrapper that sets JAVA_HOME and calls the actual script
+    # Create wrapper that sets JAVA_HOME
     (bin/"bbrun").write <<~EOS
       #!/bin/bash
       export JAVA_HOME="#{Formula["openjdk@17"].opt_prefix}"
-      exec "#{libexec}/bin/bbrun-cli" "$@"
+      exec "#{libexec}/bin/bbrun" "$@"
     EOS
     chmod 0755, bin/"bbrun"
   end
